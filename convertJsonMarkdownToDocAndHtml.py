9263cda6c98fbf48b9f5ac7fd97f5051969bd1cc
Sendhil RANDON <randon2006@gmail.com>
2024-12-05 10:19:38 +0100

    finalized

--- convertJsonMarkdownToDocAndHtml.py
+++ convertJsonMarkdownToDocAndHtml.py
@@ -1,69 +1,51 @@
 import json
-import time
 from docx import Document
 import re
 import markdown
-from html2docx import html2docx
-import markdown
-from docx import Document
 from bs4 import BeautifulSoup
 import os
 
 
 class ConvertGsscData:
-    jsonData = None
     ret = ""
+    json_data = ""
 
-    def __init__(self, inputFile_path, outputFile_path):
-        print(self.getFilesList(inputFile_path))
-        chunk = [i for i in (self.getFilesList(inputFile_path))]
-        try:
-            for file in self.getFilesList(inputFile_path):
-                self.jsonData = self.check_pdf_field(f"{inputFile_path}/{file}")
-                self.extractData(f"{outputFile_path}/{file.split(".")[0]}")
-        except FileNotFoundError:
-            print(f"The file {chunk} was not found!")
+    def __init__(self, **kwargs):
+        self.input_file_path = kwargs.get("input_file_path", "")
+        self.output_file_path = kwargs.get("output_file_path", "")
+        self.appName = kwargs.get("appName", "")
 
-    def getFilesList(self, folder_path):
-        file_list = [
+    def get_files_list(self, folder_path):
+        return [
             f
             for f in os.listdir(folder_path)
             if os.path.isfile(os.path.join(folder_path, f))
         ]
-        return file_list
 
-    def extractData(self, output_file):
+    def extract_data(self, output_file):
         added_titles = set()
-        for i in self.jsonData[2]["data"]:
-            if i["appName"] not in added_titles:
-                added_titles.add(i["appName"])
-                self.filter_and_sort_by_appname(i["appName"])
-        html_content = markdown.markdown(self.ret)
-        self.convertToHtmlContent(html_content, f"{output_file}.html")
-        self.convertToDoc(html_content, f"{output_file}.docx")
-
-    def extractData_54_56(self, output_file):
-        added_titles = set()
-        for i in self.jsonData[2]["data"]:
-            if i["appName"] not in added_titles:
-                added_titles.add(i["appName"])
-                self.filter_and_sort_by_appname(i["appName"])
-        html_content = markdown.markdown(self.ret)
-        self.convertToHtmlContent(html_content, f"{output_file}.html")
-        self.convertToDoc(html_content, f"{output_file}.docx")
+        for item in self.json_data:
+            if item["appName"] not in added_titles:
+                added_titles.add(item["appName"])
+                self.filter_and_sort_by_appname(item["appName"])
+        html_content = markdown.markdown(self.get_markdown_content())
+        self.convert_to_html_content(html_content, f"{output_file}.html")
+        self.convert_to_doc(html_content, f"{output_file}.docx")
 
     def check_pdf_field(self, filepath):
-        with open(filepath, "r", encoding="utf-8") as file:
-            data = json.load(file)
-            return data
+        try:
+            with open(filepath, "r", encoding="utf-8") as file:
+                return json.load(file)
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
+            return []
 
     def remove_characters(self, text):
         return re.sub(r"[\x00-\x08\x0B\x0C\x0E-\x1F]", "", text)
 
-    # Extract relevant data (appName, nom, contenu)
     def filter_and_sort_by_appname(self, app_name):
         extracted_data = []
-        for entry in self.jsonData[2]["data"]:
+        for entry in self.json_data:
             if entry["appName"] == app_name:
                 nom = entry.get("nom", "")
                 extracted_data.append(
@@ -73,12 +55,8 @@ class ConvertGsscData:
                         "contenu": entry["contenu"],
                     }
                 )
-        # Sort the filtered data by 'nom'
         sorted_data = sorted(extracted_data, key=lambda x: x["nom"])
-
-        # Display the sorted data
         for item in sorted_data:
-            # encoded_string = dirty_string.encode("utf-8", "ignore")
             nom = item.get("nom", "")
             self.ret += (
                 f"\n#{self.remove_characters(item['appName'])}"
@@ -89,50 +67,36 @@ class ConvertGsscData:
     def remove_invalid_xml_chars(self, s):
         return re.sub(r"[\x00-\x08\x0B-\x0C\x0E-\x1F]", "", s)
 
-    # Function to convert Markdown to DOCX
-    def convertToDoc(self, markdown_content, output_file):
+    def convert_to_doc(self, markdown_content, output_file):
         html_content = markdown.markdown(markdown_content)
-
         soup = BeautifulSoup(html_content, "html.parser")
-
         doc = Document()
-
         for element in soup.children:
             try:
                 if element.name == "h1":
-                    # Convert <h1> to Heading level 1 in Word
                     doc.add_heading(element.get_text(), 0)
                 elif element.name == "h2":
-                    # Convert <h2> to Heading level 2 in Word
                     doc.add_heading(element.get_text(), level=2)
                 elif element.name == "h3":
-                    # Convert <h3> to Heading level 3 in Word
                     doc.add_heading(element.get_text(), level=3)
                 elif element.name == "p":
-                    # Convert <p> to a paragraph in Word
                     doc.add_paragraph(element.get_text())
                 elif element.name == "ul":
-                    # Convert <ul> (unordered list) to a Word list
                     for li in element.find_all("li"):
                         doc.add_paragraph(f"- {li.get_text()}", style="List Bullet")
                 elif element.name == "ol":
-                    # Convert <ol> (ordered list) to a Word list
                     for li in element.find_all("li"):
                         doc.add_paragraph(f"{li.get_text()}", style="List Number")
                 elif element.name == "a":
-                    # Handle links
                     text = element.get_text()
                     link = element.get("href")
-                    doc.add_paragraph(
-                        f"{text} ({link})"
-                    )  # In this case, just add a plain link
+                    doc.add_paragraph(f"{text} ({link})")
             except ReferenceError:
                 print("Value error")
         print(output_file)
         doc.save(output_file)
 
-    # Wrap the HTML content in basic HTML structure
-    def convertToHtmlContent(self, html_content, output_html):
+    def convert_to_html_content(self, html_content, output_html):
         html_page = f"""
       <!DOCTYPE html>
       <html lang="en">
@@ -147,19 +111,15 @@ class ConvertGsscData:
       </body>
       </html>
       """
-
-        # Save the HTML content to a file
         with open(output_html, "w", encoding="utf-8") as f:
             f.write(html_page)
         print(f"Markdown has been converted to an HTML page: {output_html}")
 
-
-# End of class
-
-
-input_file_1_3 = "jsonToExtract_1_3"
-input_file_54_56 = "jsonToExtract_54_56"
-folderOutput_path = "extracted"
-
-ConvertGsscData(input_file_1_3, folderOutput_path)
-ConvertGsscData(input_file_54_56, folderOutput_path)
+    def run(self):
+        try:
+            self.json_data = self.check_pdf_field(
+                f"{self.input_file_path}/{self.appName}.json"
+            )
+            self.extract_data(f"{self.output_file_path}/{self.appName.split('.')[0]}")
+        except FileNotFoundError:
+            print(f"The file '{self.appName}' was not found!")
--- extractArticle.py
+++ extractArticle.py
@@ -1,42 +1,82 @@
 import mysql.connector
+import json
+from datetime import datetime
+from convertJsonMarkdownToDocAndHtml import ConvertGsscData
 
-# Replace these values with your database information
-config = {
-	'user': 'root',
-	'password': '',
-	'host': 'localhost',
-	'database': 'gssc_articles',
-}
 
-try:
-	# Establish the connection
-	connection = mysql.connector.connect(**config)
+class ExtractSqlData(ConvertGsscData):
+    query = None
 
-	if connection.is_connected():
-		print("Successfully connected to the database")
+    def __init__(self, query, **kwargs):
+        self.query = query
+        self.appName = kwargs.get("appName")
+        super().__init__(**kwargs)
 
-		# Create a cursor object using the cursor() method
-		cursor = connection.cursor()
+    def connectDbExtract(self):
+        # Replace these values with your database information
+        config = {
+            "user": "root",
+            "password": "",
+            "host": "localhost",
+            "database": "gssc",
+        }
 
-		# Write the SQL query to retrieve data
-		sql_query = "SELECT * FROM your_table"
+        try:
+            # Establish the connection
+            connection = mysql.connector.connect(**config)
 
-		# Execute the query
-		cursor.execute(sql_query)
+            if connection.is_connected():
+                print("Successfully connected to the database")
 
-		# Fetch all rows from the executed query
-		results = cursor.fetchall()
+                cursor = connection.cursor()
+                sql_query = self.query
+                cursor.execute(sql_query)
+                results = cursor.fetchall()
+                columns = [description[0] for description in cursor.description]
 
-		# Iterate through the results and print them
-		for row in results:
-			print(row)
+                result = [dict(zip(columns, row)) for row in results]
+                for row in result:
+                    # Check if 'date_de_publication' key exists in the row and remove it
+                    for key in ["date_de_publication", "derniere_modif"]:
+                        row.pop(key, None)
 
-except mysql.connector.Error as err:
-	print(f"Error: {err}")
-finally:
-	# Close the cursor and connection
-	if 'cursor' in locals():
-		cursor.close()
-	if 'connection' in locals() and connection.is_connected():
-		connection.close()
-		print("Database connection closed.")
+                # Write the result to a JSON file
+                output = f"{self.input_file_path}/{self.appName}.json"
+                with open(output, "w", encoding="utf-8") as json_file:
+                    json.dump(result, json_file, indent=4, default=str)
+
+                print(f"Query executed successfully. Results saved to {self.appName}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            # Close the connection
+            if connection:
+                connection.close()
+
+    def run(self):
+        self.connectDbExtract()
+        super().run()
+
+
+if __name__ == "__main__":
+    input_path = "json_application"
+    folder_output_path = "extracted"
+    try:
+        # Open and read the JSON file
+        with open("query/query.json", "r") as file:
+            data = json.load(file)
+        # Iterate over the list of apps and print the "appname" for each app
+        for q in data["queries"]:
+            appName = q["appName"]
+            query = q["query"]
+            executed = ExtractSqlData(
+                query=query,
+                appName=appName,
+                input_file_path=input_path,
+                output_file_path=folder_output_path,
+            ).run()
+
+    except FileNotFoundError:
+        print("The file 'data.json' was not found.")
+    except json.JSONDecodeError:
+        print("There was an error decoding the JSON data.")
